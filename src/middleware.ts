be18a899982870a1bde81014296d68e021082f44
ErandaMadusanka <emsamarasinghe@gmail.com>
2024-09-15 15:18:24 +0530

    fix github, google & password authentication

---
+++ src/middleware.ts
@@ -0,0 +1,24 @@
+import {
+  convexAuthNextjsMiddleware,
+  createRouteMatcher,
+  isAuthenticatedNextjs,
+  nextjsMiddlewareRedirect,
+} from "@convex-dev/auth/nextjs/server";
+
+const isSignInPage = createRouteMatcher(["/auth"]);
+const isProtectedRoute = createRouteMatcher(["/", "/test(.*)"]);
+
+export default convexAuthNextjsMiddleware((request) => {
+  if (isSignInPage(request) && isAuthenticatedNextjs()) {
+    return nextjsMiddlewareRedirect(request, "/");
+  }
+  if (isProtectedRoute(request) && !isAuthenticatedNextjs()) {
+    return nextjsMiddlewareRedirect(request, "/auth");
+  }
+});
+ 
+export const config = {
+  // The following matcher runs middleware on all routes
+  // except static assets.
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
