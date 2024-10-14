07a83376a6e9345681a55e798b9dfb549fb47eca
ErandaMadusanka <emsamarasinghe@gmail.com>
2024-10-14 11:27:08 +0530

    fix editor UI and functions

--- src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
@@ -3,6 +3,7 @@
 import { useChannelId } from "@/app/hooks/use-channel-id";
 import { useGetChannel } from "@/features/channels/api/use-get-channel";
 import { Loader, TriangleAlert } from "lucide-react";
+import { ChatInput } from "./chat-input";
 import { Header } from "./header";
 
 const ChannelIdPage = () => {
@@ -30,6 +31,9 @@ const ChannelIdPage = () => {
    return (
       <div className="flex flex-col h-full">
          <Header title={channel.name} />
+         <div className="flex mt-auto">
+            <ChatInput placeholder={`Message # ${channel.name}`} />
+         </div>
       </div>
    );
 };
