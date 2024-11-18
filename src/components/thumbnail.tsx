afba23a8f139c8bc5fcc6c69048f9906f074fab3
ErandaMadusanka <emsamarasinghe@gmail.com>
2024-11-18 15:57:15 +0530

    fix message image click & zoom

--- src/components/thumbnail.tsx
+++ src/components/thumbnail.tsx
@@ -9,8 +9,15 @@ export const Thumbnail = ({ url }: ThumbanilProps) => {
    if (!url) return null;
 
    return (
-      <div className="relative overflow-hidden max-w-[360px] border rounded-lg my-2 cursor-zoom-in">
-         <img src={url} alt="Message image" className="rounded-md object-cover size-full" />
-      </div>
+      <Dialog>
+         <DialogTrigger>
+            <div className="relative overflow-hidden max-w-[360px] border rounded-lg my-2 cursor-zoom-in">
+               <img src={url} alt="Message image" className="rounded-md object-cover size-full" />
+            </div>
+         </DialogTrigger>
+         <DialogContent className="max-w-[800px] border-none bg-transparent p-0 shadow-none">
+            <img src={url} alt="Message image" className="rounded-md object-cover size-full" />
+         </DialogContent>
+      </Dialog>
    );
 };
